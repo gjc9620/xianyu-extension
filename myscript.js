3443317b3bdbeef56dc2a91f46898ee399050b55
gjc9620 <992758355@qq.com>
2017-08-26 12:52:03 +0800

    search

---
+++ myscript.js
@@ -0,0 +1,3 @@
+var J_IdleHeader = document.querySelector('#J_IdleHeader');
+J_IdleHeader.innerHTML = J_IdleHeader.innerHTML.replace(/<!--|-->/g, "");
+document.querySelector('.search-filters').style.display = 'inherit';
